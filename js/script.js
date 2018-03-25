54132e1b0e6afb979da5675f543e583341d47621
Vlad <vladyslavyantsov@gmail.com>
2018-03-25 17:59:43 +0200

    landing page first step animation done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//var hamburger = document.querySelector('.hamburger');
+//        
+//hamburger.addEventListener("click",showMenu);
+//        
+//function showMenu(){
+//    this.nextElementSibling.querySelector('nav ul').style.display = "block";
+//}
